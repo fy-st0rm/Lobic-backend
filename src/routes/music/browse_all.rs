c7a7f9cb71fe2fa2f86dfc79bf012146e571dea2
Rasaili-rain <saditrasaili063@gmail.com>
2025-01-28 01:18:42 +0545

    :send a array for browse :catagory

--- src/routes/music/browse_all.rs
+++ src/routes/music/browse_all.rs
@@ -1,18 +1,10 @@
+use crate::core::app_state::AppState;
 use axum::{
 	extract::{Path, State},
 	http::{header, StatusCode},
 	response::Response,
 };
 use diesel::prelude::*;
-use serde::Serialize;
-
-use crate::core::app_state::AppState;
-
-#[derive(Serialize)]
-pub struct BrowseResponse {
-	key: String,
-	values: Option<Vec<String>>,
-}
 
 pub async fn browse_all(State(app_state): State<AppState>, Path(category): Path<String>) -> Response<String> {
 	let mut db_conn = match app_state.db_pool.get() {
@@ -41,35 +33,17 @@ pub async fn browse_all(State(app_state): State<AppState>, Path(category): Path<
 	};
 
 	match result {
-		Ok(items) => {
-			let response = match category.as_str() {
-				"artists" => BrowseResponse {
-					key: String::from("artists"),
-					values: Some(items),
-				},
-				"albums" => BrowseResponse {
-					key: String::from("albums"),
-					values: Some(items),
-				},
-				"genres" => BrowseResponse {
-					key: String::from("genres"),
-					values: Some(items),
-				},
-				_ => unreachable!(), // This case is already handled above
-			};
-
-			match serde_json::to_string(&response) {
-				Ok(json) => Response::builder()
-					.status(StatusCode::OK)
-					.header(header::CONTENT_TYPE, "application/json")
-					.body(json)
-					.unwrap(),
-				Err(err) => Response::builder()
-					.status(StatusCode::INTERNAL_SERVER_ERROR)
-					.body(format!("Failed to serialize response: {err}"))
-					.unwrap(),
-			}
-		}
+		Ok(items) => match serde_json::to_string(&items) {
+			Ok(json) => Response::builder()
+				.status(StatusCode::OK)
+				.header(header::CONTENT_TYPE, "application/json")
+				.body(json)
+				.unwrap(),
+			Err(err) => Response::builder()
+				.status(StatusCode::INTERNAL_SERVER_ERROR)
+				.body(format!("Failed to serialize response: {err}"))
+				.unwrap(),
+		},
 		Err(err) => Response::builder()
 			.status(StatusCode::INTERNAL_SERVER_ERROR)
 			.body(format!("Database error: {err}"))

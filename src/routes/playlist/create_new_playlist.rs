bee21235521e0aa1c8f451ea6bbf8996a7cfec20
Rasaili-rain <saditrasaili063@gmail.com>
2025-02-18 09:28:50 -0800

    playtlist cover image

--- src/routes/playlist/create_new_playlist.rs
+++ src/routes/playlist/create_new_playlist.rs
@@ -1,13 +1,21 @@
-use crate::core::app_state::AppState;
+use std::fs;
+use std::path::Path;
+
 use crate::lobic_db::models::Playlist;
-use axum::{extract::State, http::status::StatusCode, response::Response, Json};
+use crate::{config::PLAYLIST_COVER_IMG_STORAGE, core::app_state::AppState};
+use axum::{
+	body::Bytes,
+	extract::{Query, State},
+	http::status::StatusCode,
+	response::Response,
+};
 use chrono::Utc;
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct NewPlaylist {
+pub struct PlaylistParams {
 	pub playlist_name: String,
 	pub user_id: String,
 	pub is_playlist_combined: bool,
@@ -18,7 +26,11 @@ pub struct ApiResponse {
 	pub message: String,
 }
 
-pub async fn create_playlist(State(app_state): State<AppState>, Json(payload): Json<NewPlaylist>) -> Response<String> {
+pub async fn create_playlist(
+	State(app_state): State<AppState>,
+	Query(params): Query<PlaylistParams>,
+	body: Bytes,
+) -> Response<String> {
 	let mut db_conn = match app_state.db_pool.get() {
 		Ok(conn) => conn,
 		Err(err) => {
@@ -40,12 +52,28 @@ pub async fn create_playlist(State(app_state): State<AppState>, Json(payload): J
 
 	let new_playlist = Playlist {
 		playlist_id: curr_playlist_id.to_string(),
-		playlist_name: payload.playlist_name,
-		user_id: payload.user_id,
+		playlist_name: params.playlist_name,
+		user_id: params.user_id,
 		creation_date_time: curr_creation_date_time.clone(),
 		last_updated_date_time: curr_creation_date_time,
-		is_playlist_combined: payload.is_playlist_combined,
+		is_playlist_combined: params.is_playlist_combined,
 	};
+
+	//save the image inside the storage
+	let storage_path = Path::new(PLAYLIST_COVER_IMG_STORAGE);
+	if let Err(err) = fs::create_dir_all(storage_path) {
+		return Response::builder()
+			.status(StatusCode::INTERNAL_SERVER_ERROR)
+			.body(format!("Failed to create directory: {}", err))
+			.unwrap();
+	}
+	let image_path = storage_path.join(format!("{}.png", curr_playlist_id));
+	if let Err(err) = fs::write(&image_path, body) {
+		return Response::builder()
+			.status(StatusCode::INTERNAL_SERVER_ERROR)
+			.body(format!("Failed to save image: {}", err))
+			.unwrap();
+	}
 
 	match diesel::insert_into(playlists)
 		.values(&new_playlist)
